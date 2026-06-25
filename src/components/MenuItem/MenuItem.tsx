df8a537bf711713b946900168c9dbf487c97b7ee
Yash Pandya <ypandya@cm2.cm2solutions.com>
2026-06-25 15:50:22 -0400

    feat: add recursive navigation dropdowns

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,19 +1,27 @@
 import type { NavigationItem } from "../../types/Menu";
+import "./MenuItem.css";
 
-type NavigationItemProps = {
+type MenuItemProps = {
     item: NavigationItem;
 };
 
-export default function MenuItem({ item }: NavigationItemProps) {
-    return <div className="menu-item">
-        <a href={item.link || "#"}>
-            {item.label}
-        </a>
+export default function MenuItem({ item }: MenuItemProps) {
+    const hasChildren = item.children && item.children.length > 0;
 
-        {item.children && (
-            <div className="submenu">{item.children?.map((child) => (
-                <MenuItem key={child.label} item={child} />
-            ))}</div>
-        )}
-    </div >
+    return (
+        <div className="menu-item">
+            <a className="menu-link" href={item.link ?? "#"}>
+                {item.label}
+                {hasChildren && <span className="menu-caret">▾</span>}
+            </a>
+
+            {hasChildren && (
+                <div className="submenu">
+                    {item.children!.map((child) => (
+                        <MenuItem key={child.label} item={child} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
